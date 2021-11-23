3baff32e2b1333ee1b13befb471c184962f8a66d
ZussM4n13 <diegoromerofuentes@gmail.com>
2021-11-23 18:28:26 +0100

    Agregado el archivo del proyecto del precioBitcoin.py

---
+++ precioBitcoin.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+import schedule
+
+TOKEN = '2071545023:AAHdU-bdQ285L4YYU4HpZ6NvUJhZ3Q908gA'
+chatId = '1442996092'
+
+#<td class="wbreak_word align-middle coin_price">$60,832.60</td>
+
+def btc_scrapping():
+    url_bitcoin = 'https://awebanalysis.com/es/coin-details/bitcoin/'
+    url = requests.get(url_bitcoin)
+    soup = BeautifulSoup(url.content, 'html.parser')
+    result = soup.find('td', {'class' : 'coin_price'})
+    format_result = result.text
+    #print(format_result)
+
+    return format_result
+
+#https://api.telegram.org/bot2071545023:AAHdU-bdQ285L4YYU4HpZ6NvUJhZ3Q908gA/getUpdates
+
+def bot_send_text(bot_message):
+    send_text = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + chatId + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+    return response
+
+#test_bot = bot_send_text('/start')
+#print(test_bot)
+
+def report():
+    btc_price = f'El precio del Bitcoin es de {btc_scrapping()}'
+    bot_send_text(btc_price)
+
+if __name__ == '__main__':
+
+    schedule.every().day.at("18:24").do(report)
+
+    while True:
+        
+        schedule.run_pending()
